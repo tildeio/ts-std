f41a013024a47e4d31b6bcb80c6c2a990d031ae4
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-07-23 23:27:56 -0700

    Ensure all keys described only return strings.

--- src/object.ts
+++ src/object.ts
@@ -1,14 +1,14 @@
-export function keys<T extends object>(obj: T): Array<keyof T> {
+export function keys<T extends object>(obj: T): Array<string & keyof T> {
   return Object.keys(obj) as any;
 }
 
-export function values<T extends object>(obj: T): Array<T[keyof T]> {
+export function values<T extends object>(obj: T): Array<T[string & keyof T]> {
   return keys(obj).map(k => obj[k]) as any;
 }
 
 const { hasOwnProperty } = Object.prototype;
 
-export function* entries<T extends object>(obj: T): Iterable<[keyof T, T[keyof T]]> {
+export function* entries<T extends object>(obj: T): Iterable<[string & keyof T, T[string & keyof T]]> {
   for (let key in obj) {
     if (hasOwnProperty.call(obj, key)) {
       yield [key, obj[key]];
